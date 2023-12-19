31bc974c1df8a2ab85119735466942973bebd75b
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-19 16:35:08 +0700

    Create entities

---
+++ src/main/java/com/ray/entity/User.java
@@ -0,0 +1,85 @@
+package com.ray.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Integer userId;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "full_name")
+	private String fullName;
+	
+	public User() {}
+	
+	public User(String email, String password, String fullName) {
+		this.email = email;
+		this.password = password;
+		this.fullName = fullName;
+	}
+	
+	public User(Integer userId, String email, String password, String fullName) {
+		this.userId = userId;
+		this.email = email;
+		this.password = password;
+		this.fullName = fullName;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId 
+				+ ", email=" + email 
+				+ ", password=" + password 
+				+ ", fullName=" + fullName
+				+ "]";
+	}
+	
+	
+}
